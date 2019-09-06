30fc495d62bc93f6133b392d495eeefb98ea953e
nil <Nilrajsinh6005@ICLOUD.COM>
2019-09-06 13:30:14 +0530

    Initial Commit

---
+++ Expance Managment AppTests/Expance_Managment_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Expance_Managment_AppTests.swift
+//  Expance Managment AppTests
+//
+//  Created by Nilrajsinh Vaghela on 06/09/19.
+//  Copyright © 2019 Cyphers. All rights reserved.
+//
+
+import XCTest
+@testable import Expance_Managment_App
+
+class Expance_Managment_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
